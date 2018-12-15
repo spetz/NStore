3ea713055bafa18a251e0fea6fe565687bd68f43
Piotr Gankiewicz <spetz90@gmail.com>
2018-12-15 13:57:10 +0100

    Product details

---
+++ src/NStore.Web/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NStore.Web.ViewModels;
+
+namespace NStore.Web.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ProductsProvider _productsProvider;
+        
+        public ProductViewModel Product { get; private set; }
+
+        public DetailsModel(ProductsProvider productsProvider)
+        {
+            _productsProvider = productsProvider;
+        }
+        
+        public IActionResult OnGet(Guid id)
+        {
+            Product = _productsProvider.Products.SingleOrDefault(p => p.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
