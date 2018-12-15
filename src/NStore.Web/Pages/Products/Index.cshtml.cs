6561a6d6cf31057373a50e2e9d289f3ea31fd160
Piotr Gankiewicz <spetz90@gmail.com>
2018-12-15 12:28:50 +0100

    products view

--- src/NStore.Web/Pages/Products/Index.cshtml.cs
+++ src/NStore.Web/Pages/Products/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NStore.Web.ViewModels;
 
 namespace NStore.Web.Pages.Products
@@ -10,6 +13,15 @@ namespace NStore.Web.Pages.Products
         
         public IEnumerable<ProductViewModel> Products { get; private set; }
 
+        public IEnumerable<SelectListItem> Categories => new List<SelectListItem>
+        {
+            new SelectListItem("Phones", "Phones"),
+            new SelectListItem("Computers", "Computers")
+        };
+
+        [BindProperty]
+        public ProductViewModel Product { get; set; } = new ProductViewModel();
+
         public IndexModel(ProductsProvider productsProvider)
         {
             _productsProvider = productsProvider;
@@ -18,6 +30,18 @@ namespace NStore.Web.Pages.Products
         public void OnGet()
         {
             Products = _productsProvider.Products;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Product.Id = Guid.NewGuid();
+            _productsProvider.Products.Add(Product);
+
+            return RedirectToPage();
         }
     }
 }
